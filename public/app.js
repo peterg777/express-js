b45cc4302cea9260eab92faf5390b206760acb3e
peterg777 <ptrg777@gmail.com>
2020-04-22 14:59:35 -0400

    added serverjs and finished form

--- public/app.js
+++ public/app.js
@@ -1,22 +1,22 @@
-document.getElementById('theForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    let url = '../formsubmissions.json';
+// document.getElementById('theForm').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     let url = '../formsubmissions.json';
 
-    let name = document.getElementById('input1').value;
-    let email = document.getElementById('input2').value;
-    let inputs = {
-        name: `${name}`,
-        email: `${email}`
-    };
+//     let name = document.getElementById('input1').value;
+//     let email = document.getElementById('input2').value;
+//     let inputs = {
+//         name: `${name}`,
+//         email: `${email}`
+//     };
     
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(inputs),
+//     fetch(url, {
+//         method: 'POST',
+//         body: JSON.stringify(inputs),
         
   
-        })
-    }).then((res) => {
-        console.log(res.status);
-    }).catch((err) => {
-        console.log(err);
-    });
+//         })
+//     }).then((res) => {
+//         console.log(res.status);
+//     }).catch((err) => {
+//         console.log(err);
+//     });
--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const formSubmit = path.join(__dirname, '../formsubmissions.json')
 let app = express();
-app.use((req,res,next)=> {
+app.use((req, res, next) => {
     console.log(req.method + ' ' + req.url)
     next()
 })
 app.use(express.static('public'))
 app.get('/test', (req, res) => {
     res.send('hello from the web server side!!!!!!!!!!');
-  
+
 });
 
+app.use(express.urlencoded({extended:false}))
+// app.use(express.json())
+app.post("/formsubmission", (req, res) => {
+    
+    let name = req.body.name;
+    let email = req.body.email;
+    let formData = {
+           name,
+           email
+    }; //adds submitted firstName and lastName to formData
+    console.log(formData)
+    fs.readFile(path.join(__dirname, "./formsubmissions.json"), (err, data) => {
+        if (err) console.log(err); //log errors
+        let parsedData = JSON.parse(data); //data that is parsed into JSON is called parsedData
+        parsedData.push(formData); //pushes parsedData into array
+        fs.writeFile(
+            path.join(__dirname, "./formsubmissions.json"),
+            JSON.stringify(parsedData),
+            err => {
+                if (err) console.log(err);
+                res.send(parsedData);
+            }
+        );
+    }
+    );
+});
 
-//     let formValue = JSON.stringify(req.body)
-//     fs.writeFile(formSubmit, formValue, (err) => {
-//         if (err) {console.log(err);} 
-//    })
-//     res.send('Information Received!!!!');
-
-
-app.listen(3000,() => console.log('server running on port 3000') );
+app.listen(3000, () => console.log('server running on port 3000'));
 
